d30660472f6fe11305c18f6219dc0b23e6bd4676
Eduardo <eduardoxer@gmail.com>
2023-10-03 19:12:53 -0300

    Estoque Forte Versao 0.1

---
+++ js/script.js
@@ -0,0 +1,16 @@
+ /*function sexo() {
+    return console.log("sexo")
+}
+
+
+
+var album = {
+    title: "NX Zero",
+    released: 2011,
+    showInfo: function() {
+      alert("Título do álbum: " + this.title + "Lançado em: " + this.released);
+    }
+  };
+  
+album.showInfo()
+*/
